c5f1ee0cddea3a83cf88dc1578f1b4c67d90948d
buchmayne <michaelmbuchman@gmail.com>
2020-10-28 15:44:56 -0700

    added headers for get request

--- craigslist.py
+++ craigslist.py
@@ -2,10 +2,14 @@ from requests import get
 from bs4 import BeautifulSoup
 import pandas as pd
 
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
+}
+
 # (ADU)-"this is not adu"-"this is not an adu unit"|("accessory dwelling unit")|("In-law")|("granny flat")|("backyard cottage")|
 craigslist_search_results = "https://portland.craigslist.org/search/apa?query=%28ADU%29-%22this+is+not+adu%22-%22this+is+not+an+adu+unit%22%7C%28%22accessory+dwelling+unit%22%29%7C%28%22In-law%22%29%7C%28%22granny+flat%22%29%7C%28%22backyard+cottage%22%29%7C&availabilityMode=0&sale_date=all+dates"
 
-search_results = get(url=craigslist_search_results)
+search_results = get(url=craigslist_search_results, headers=headers)
 
 search_results_soup = BeautifulSoup(search_results.content, "html.parser")
 
