53a1d76e3754cd97f82d9793688504f420a1f200
buchmayne <michaelmbuchman@gmail.com>
2020-10-29 16:45:09 -0700

    getting links of apartments from first page of apartments.com

--- apartments.py
+++ apartments.py
@@ -1,15 +1,18 @@
-import requests
+from requests import get
 from bs4 import BeautifulSoup
 
-base_url = "https://www.apartments.com/portland-or/"
+portland_apartments_url = "https://www.apartments.com/portland-or/"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
 }
 
-r = requests.get(base_url, headers=headers)
-c = r.content
+response = get(portland_apartments_url, headers=headers)
+content = response.content
 
-soup = BeautifulSoup(c, "html.parser")
+soup = BeautifulSoup(content, "html.parser")
 
-print(soup)
+links = [link["href"] for link in soup.find_all("a", class_="property-link")]
+unique_links = list(set(links))
+
+unique_links
