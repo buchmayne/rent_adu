ef9235883689bf1977e8e214b4c5c59b686dce00
buchmayne <michaelmbuchman@gmail.com>
2020-11-22 16:37:11 -0800

    set up connection to AWS RDS, scraped initial results there

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
+import credentials as creds
 from scrapers import craigslist as cl
-import credentials
+
 
 if __name__ == "__main__":
     urls_to_scrape = cl.get_list_of_all_urls_to_scrape(
@@ -16,7 +17,15 @@ if __name__ == "__main__":
         portland_url_mask=cl.portland_url_mask,
     )
 
-    engine = create_engine(credentials.shred_connection_string)
+    connection_string = "postgres://{}:{}@{}:{}/{}".format(
+        creds.aws_user,
+        creds.aws_password,
+        creds.aws_host,
+        creds.aws_port,
+        creds.aws_database,
+    )
+
+    engine = create_engine(connection_string)
     listings_data.to_sql(
         "craigslist_adu_rent", con=engine, if_exists="append", index=False
     )
