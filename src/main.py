a9ef372e19063c1233aeca0469bac3786c874c91
buchmayne <michaelmbuchman@gmail.com>
2020-11-29 17:16:05 -0800

    access aws rds connection string through s3

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
-from credentials import connection_string
 import craigslist as cl
+import boto3
+from io import BytesIO
 
 
 if __name__ == "__main__":
@@ -16,6 +17,15 @@ if __name__ == "__main__":
         portland_url_slice=cl.portland_url_slice,
         portland_url_mask=cl.portland_url_mask,
     )
+
+    # get connection string to aws rds from s3 bucket
+    session = boto3.Session()
+    s3_client = session.client("s3")
+    f = BytesIO()
+    s3_client.download_fileobj(
+        "rent-adu-credentials-bucket", "connection_string.txt", f
+    )
+    connection_string = f.getvalue().decode("UTF-8")
 
     engine = create_engine(connection_string)
     listings_data.to_sql("cl_adu_rent", con=engine, if_exists="append", index=False)
